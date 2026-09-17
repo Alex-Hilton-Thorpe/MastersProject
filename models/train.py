3cd2e49d5b622bb1bd0f96bedd4289ad54cd5e72
Alex-Hilton-Thorpe <25934171@sun.ac.za>
2026-09-17 15:22:32 +0200

    Complete training portion of the first CNN through PyTorch

--- models/train.py
+++ models/train.py
@@ -25,4 +25,58 @@ train_loader = DataLoader(
     shuffle=True
 )
 
+class CNN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=16,
+            kernel_size=3,
+        )
+
+        self.relu = nn.ReLU()
+
+        self.pool = nn.MaxPool2d(kernel_size=2)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear((16 * 63 * 63), 6) 
+        # This is a linear layer that takes the flattened output of the convolutional layers and maps it to 6 output classes 
+        # Basically 16 = out_channels, 63 = (128 - 3 + 1) / 2 = 63 (after convolution (-3+1) and pooling (/2))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        return x
+
+"""These all get called before loop to initialize the model, optimizer, and loss function."""
+model = CNN()
+
+optimizer = optim.Adam(
+    model.parameters(),
+    lr = LEARNING_RATE
+)
+
+criterion = nn.CrossEntropyLoss()
+# CrossEntropyLoss is commonly used for multi-class classification.
+# It compares the model's logits with the correct class labels
+# and produces a single loss value. 
+
+for epoch in range(EPOCHS):
+    epoch_loss = 0.0
+    
+    for images, labels in train_loader:
+        optimizer.zero_grad() # Clears previous gradient else the gradients would accumulate across multiple backward passes
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward() # Backpropagation: computes the gradient of the loss with respect to the model's parameters
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    print(f"Epoch {epoch + 1} Loss: {epoch_loss/len(train_loader)}")
 
